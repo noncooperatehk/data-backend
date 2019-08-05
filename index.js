8abcce374380bde429839de2250b8c438818ba52
noncooperatehk <53223012+noncooperatehk@users.noreply.github.com>
2019-08-05 16:36:26 +0800

    add functionality to update markdown json and AST json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import {highLevelParser} from "noncooperatehk-data-handler";
-import indexJson from "./index.json"
-import fs from "fs";
-import _ from "lodash";
+import {highLevelParser} from 'noncooperatehk-data-handler';
+import indexJson from './index.json'
+import fs from 'fs';
+import _ from 'lodash';
 
 const fsPromises = fs.promises;
 
@@ -10,24 +10,46 @@ if (process.argv.length < 3) {
   process.exit(1);
 }
 
-let filName = process.argv[2];
+let fileName = process.argv[2];
 
 async function main(fileName) {
   let fullPath = `${__dirname}/${fileName}`;
   let [frontMatterJson, ast, mdStr] = await highLevelParser.parseFile(fullPath);
+  if (!_.get(frontMatterJson, 'id')) throw new FormatException(`missing id in frontMatter. received: ${frontMatterJson}`);
+
   await updateIndexJson(frontMatterJson);
+  console.log('index.json updated')
+
+  await updateAstJson(frontMatterJson.id, ast);
+  console.log('ast.json updated')
+
+  await updateMarkDownJson(frontMatterJson.id, mdStr);
+  console.log('markdown updated')
+}
+
+async function updateAstJson(companyId, markdownAST) {
+  let filePath = `${__dirname}/company/ast/${companyId}.json`;
+  console.log(filePath)
+  return fsPromises.writeFile(filePath, JSON.stringify(markdownAST, null, '  '));
+}
+
+async function updateMarkDownJson(companyId, markDownStr) {
+  let filePath = `${__dirname}/company/md/${companyId}.json`;
+  let jsonContent = {
+    md: markDownStr
+  };
+  return fsPromises.writeFile(filePath, JSON.stringify(jsonContent, null, '  '));
 }
 
 async function updateIndexJson(frontMatterJson) {
-  if (!_.get(frontMatterJson, 'id')) throw new FormatException(`missing id in frontMatter. received: ${frontMatterJson}`);
   let objectWithKeyRenamed = _.mapKeys(frontMatterJson, (value, key) => key === 'id' ? 'companyId' : key);
   let filePath = `${__dirname}/index.json`;
   let updated = _.chain(indexJson)
     .filter(x => x.companyId !== frontMatterJson.id)
     .concat([objectWithKeyRenamed])
-    .sortBy("companyId")
+    .sortBy('companyId')
     .value();
-  return fsPromises.writeFile(filePath, JSON.stringify(updated, null, "  "));
+  return fsPromises.writeFile(filePath, JSON.stringify(updated, null, '  '));
 }
 
 function FormatException(message) {
@@ -35,4 +57,4 @@ function FormatException(message) {
   this.name = 'FormatException';
 }
 
-main(filName).then(r => console.log(r)).catch(e => console.log(e.stack));
+main(fileName).then(r => console.log(`done`)).catch(e => console.log(e.stack));
